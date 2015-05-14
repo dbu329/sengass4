bb4a531604ac47ff8537297e7fd9742feed2112a
danielbu329 <daniel.bu329@gmail.com>
2015-05-14 14:24:07 +1000

    getters and setters for flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,4 @@
+import java.sql.Time;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -12,30 +13,38 @@ public class Flight {
 	private int cost;
 	
 	public Flight() {
-		
+		date = Calendar.getInstance();
 	}
 	
-	public void setDate(Calendar cal) {
+	public void setDate(int day, int month, int year) {
+		date.set(Calendar.DAY_OF_MONTH, day);
+		date.set(Calendar.MONTH, month);
+		date.set(Calendar.YEAR, year);
+	}
+	
+	public void setTime(int hour, int minute) {
+		date.set(Calendar.HOUR, hour);
+		date.set(Calendar.MINUTE, minute);
 		
 	}
 	
 	public void setOrigin(String loc) {
-		
+		origin = loc;
 	}
 	
 	public void setDestination(String loc) {
-		
+		destination = loc;
 	}
 	
 	public void setTravelTime(int time) {
-		
+		travelTime = time;
 	}
 	
 	public void setAirline(String name) {
-		
+		airline = name;
 	}
 	
 	public void setCost(int c) {
-		
+		cost = c;
 	}
 }
