54cc2b619d48f8799536e6bbdf4556d735f48e9b
danielbu329 <daniel.bu329@gmail.com>
2015-05-14 13:04:07 +1000

    added fields Flight.java

--- src/Flight.java
+++ src/Flight.java
@@ -1,4 +1,41 @@
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class Flight {
-
+	private int id;
+	private Calendar date;
+	private String origin;
+	private String destination;
+	private int travelTime;
+	private String airline;
+	private int cost;
+	
+	public Flight() {
+		
+	}
+	
+	public void setDate(Calendar cal) {
+		
+	}
+	
+	public void setOrigin(String loc) {
+		
+	}
+	
+	public void setDestination(String loc) {
+		
+	}
+	
+	public void setTravelTime(int time) {
+		
+	}
+	
+	public void setAirline(String name) {
+		
+	}
+	
+	public void setCost(int c) {
+		
+	}
 }
