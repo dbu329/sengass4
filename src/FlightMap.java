a4084f6d2b164706b6e7f72ada59ccff824f2bb4
danielbu329 <daniel.bu329@gmail.com>
2015-05-15 00:32:56 +1000

    FlightMap is able to verfiy whether the flights are correctly formatted, also for multiple flights on one line

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -28,32 +28,40 @@ public class FlightMap {
 		try
 		{
 		    sc = new Scanner(new FileReader(file)); 
-		    Flight savedValues = new Flight();
      	    while (sc.hasNextLine()) {
      	    	Flight myFlight = new Flight();
      	    	String line = sc.nextLine();
-     	    	line.replaceAll(" ", "");
+     	    	line = line.replaceAll("\\s", "");
+
+//     	    	for (int i = 0; i < tokens.length; i++)
+//     	    		System.out.print("("+tokens[i]+")");
+     	    	System.out.println();
+     	    	//if the values are not in multiples of seven, must be missing something.
+     	    	if (!verifyFlightData(line)) {
+     	    		System.out.println("incorrectly formatted flight data");
+     	    		break;
+     	    	}
+     	    	
+     	    	line = line.replaceAll("\\[", "");
+     	    	line = line.replaceAll("\\]", ",");
      	    	String delims = "[,]+";
      	    	String tokens[] = line.split(delims);
-     	    	System.out.println(tokens.length);
-     	    	if (tokens.length % 7 != 0) {
-     	    		System.out.println("incorrectly formatted flight data"+tokens.length);
-//     	    		break;
-     	    	}
-	    		for (int i = 0; i < tokens.length; i++) {
-	 	    		//straight away start analysing
-	 	    		
-	 	    		if (tokens[i].indexOf('[') >= 0) {
-	 	    			//initiate start, if reached end of tokens, automatically
-	 	    			//read in the next line, and continues along the current FlightData
-	 	    		} 
+	    		for (int i = 0; i < tokens.length; i += 7) {
+	    			//first set date
+//	    			String[] dateTokens = tokens[i].split("/");
+	    			
+//	    			myFlight.setDate(day, month, year);
+	    			
+	    			//then set time
+	    			//then set origin and dest
+	    			//then setflight time
+	    			//set airline
+	    			//set ff points
 	    		}
-     	    	for (int i = 0; i < tokens.length; i++)
-     	    		System.out.print("("+tokens[i]+")");
+     	    	
      	    }
 		
 		}
-		
 		catch (FileNotFoundException e) {System.out.println("File not Found");}
 		finally
 		{
@@ -103,14 +111,103 @@ public class FlightMap {
 //		}
 //	}
 //	
-//	//Keep in mind the order of information in our Flight
-//	//Flight -> [ Date, Time, Name, Name, Duration, Name, Number ]
-//	private static boolean flightVerified(String[] dataArray){
-//		
-//		
-//		
-//		return true;
-//	}
+	//Keep in mind the order of information in our Flight
+	//Flight -> [ Date, Time, Name, Name, Duration, Name, Number ]
+	private boolean verifyFlightData(String data){
+		boolean valid = true;
+		
+		//First Make Sure all the Required Brackets Are there
+		if (!isValidBrackets(data))
+			valid = false;
+		//prepare data, then split data into tokens
+		data = data.replaceAll("\\s", ""); // get rid of spaces
+		data = data.replaceAll("\\[", ""); // get rid of [ 
+		data = data.replaceAll("\\]", ",");// replace [ with ,
+		String[] dataArray = data.split("[,]"); // split data by commaa (,)
 	
+		if (dataArray.length % 7 != 0) {
+			valid = false;
+		} else {
+			for (int i = 0; i < dataArray.length-1;i+=7) {
+				//1. check Date is valid
+				String[] tmpTokens = dataArray[i].split("/");
+					//if date doesn't contain 3 tokens then definitely not in layout of DD/MM/YYYY
+				if (tmpTokens.length != 3) {
+					valid = false;
+				} else {
+					//if has 3 tokens, then check if they are all integers
+					for (String s:tmpTokens) {
+						if (!isValidNumber(s)) {
+							valid = false;
+						}
+					}
+				}
+				
+				//2. check Time is valid
+				tmpTokens = dataArray[i+1].split("[:]");
+				if (tmpTokens.length != 2) {
+					valid = false;
+				} else {
+					for (String s:tmpTokens) {
+						if (!isValidNumber(s))
+							valid = false;
+					}
+				}
+				//3. check origin and destination is valid?
+				if (!isValidName(dataArray[i+2]))
+						valid = false;
+				if (!isValidName(dataArray[i+3]))
+					valid = false;
+				//4.check duration is valid?
+				if (!isValidNumber(dataArray[i+4])) 
+					valid = false;
+				//5. check Airline is valid
+				if (!isValidName(dataArray[i+5]))
+					valid = false;
+				//6. check ff points is valid
+				if (!isValidNumber(dataArray[i+6])) 
+					valid = false;
+			}
+		}
+		return valid;
+	}
+	
+	//currently a name with a space in it is VALID
+	private boolean isValidName(String name) {
+		if (!name.matches("[A-Z][a-z]+")) {
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean isValidNumber(String str) {
+		if (!str.matches("\\d+")) {
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean isValidBrackets(String data) {
+		
+		data = data.replaceAll("\\s", ""); // get rid of spaces
+		int startCount = 0, commaCount = 0, endCount = 0;
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) == '[') {
+				startCount++;
+			} else if (data.charAt(i) == ',') {
+				commaCount++;
+			} else if (data.charAt(i) == ']') {
+				if (commaCount == 6 && startCount-1 == endCount) {
+					endCount++;
+					commaCount = 0;
+				}
+			}
+		}
+		if (startCount != endCount || commaCount != 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 }
