e05fb1fd3615d4a5f99ce1fa5cbcb581eec65d80
Benjamin Mo <b.mo96@hotmail.com>
2015-05-19 21:57:59 +1000

    completed parseValidQuery function (untested) also merge w/ daniel's last commit

--- src/TravelPlan.java
+++ src/TravelPlan.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
@@ -14,7 +15,7 @@ public class TravelPlan {
 //	private static final int INCORRECT_DATE_TIME = 2;
 	
 	FlightMap myFlightMap;
-	//Query myQueryList;
+	ArrayList<Query> queryList;
 	
 	public static void main(String[] args) {
 		if (args.length != 2) {
@@ -27,6 +28,7 @@ public class TravelPlan {
 	
 	public TravelPlan(String[] args) {
 		myFlightMap = new FlightMap();
+		queryList = new ArrayList<Query>();
 		readFlightData(args[0]);
 		readQueryData(args[1]);
 	}
@@ -53,11 +55,9 @@ public class TravelPlan {
      	    	}
      	    	// edit line to split string into tokens
      	    	line = line.replaceAll("\\[", "");
-     	    	line = line.replaceAll("\\]", "");
+     	    	line = line.replaceAll("\\]", ",");
      	    	line = line.replaceAll("\\(", "");
      	    	line = line.replaceAll("\\)", "");
-     	    	// can probably do this next line instead, but untested
-//     	    	line = line.replaceAll("[\\[\\]\\(\\)]", "");
      	    	String delims = "[,]+";
      	    	String lineTokens[] = line.split(delims);
      	    	parseValidQuery(lineTokens);
@@ -69,15 +69,164 @@ public class TravelPlan {
 		}
 	}
 
-	private boolean verifyQueryFormat(String line) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	private boolean verifyQueryFormat(String data) {
+		// String data is the current line from the file
+		boolean isValid = true;
+		//First Make Sure all the Required Brackets Are there
+		if (!isValidBracketsQuery(data)) isValid = false;
+		//prepare data, then split data into tokens
+		data = data.replaceAll("\\s", ""); // get rid of spaces
+		data = data.replaceAll("\\[", ""); // get rid of [ 
+		data = data.replaceAll("\\]", ",");// replace ] with ,
+    	data = data.replaceAll("\\(", ""); // get rid of (
+    	data = data.replaceAll("\\)", ""); // get rid of )
+		String[] dataArray = data.split("[,]"); // split data by commaa (,)
 	
-	private void parseValidQuery(String[] lineTokens) {
-		// TODO Auto-generated method stub	
+		if (dataArray.length % 8 != 0) {
+			isValid = false;
+		} else {
+			for (int i = 0; i < dataArray.length-1;i+=8) {
+				//1. check Date is valid
+				String[] tmpTokens = dataArray[i].split("/");
+				//if date doesn't contain 3 tokens then definitely not in layout of DD/MM/YYYY
+				if (tmpTokens.length != 3) {
+					isValid = false;
+				} else {
+					try {
+						Integer.parseInt(tmpTokens[0]);
+						Integer.parseInt(tmpTokens[1]);
+						Integer.parseInt(tmpTokens[2]);
+					} catch(NumberFormatException e) {
+						isValid = false;
+					}
+				}
+				//2. check Time is valid
+				tmpTokens = dataArray[i+1].split("[:]");
+				if (tmpTokens.length != 2) {
+					isValid = false;
+				} else {
+					try {
+						Integer.parseInt(tmpTokens[0]);
+						Integer.parseInt(tmpTokens[1]);
+					} catch(NumberFormatException e) {
+						isValid = false;
+					}
+				}
+				//3. check origin and destination is valid?
+				if (!isValidName(dataArray[i+2])) 
+					isValid = false;
+				if (!isValidName(dataArray[i+3]))
+					isValid = false;
+				//4.check PreferenceType is valid? from i+4 to i+6
+				// daf is dis? No mention of error messages in the spec
+				// Do we actually assume that Query passed in is correct then? 
+				// If we DO do a error message for Query, are we meant to make sure
+				// that the preferenceTypes contain one of each from 
+				// (Cost, Time and "airline"?)
+				// 	Furthermore would that be an incorrect format error? or incorrect
+				//  preference error or something?
+				//TODO TODO TODO TODO TODO TODO
+				//TODO TODO TODO TODO TODO TODO TODO
+				//TODO TODO TODO TODO TODO TODO TODO TODO
+				//TODO TODO TODO TODO TODO TODO TODO TODO TODO
+				//TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+				//5. check number of plans to return is valid
+				if (!isValidNumber(dataArray[i+7])) 
+					isValid = false;
+			}
+		}
+		return isValid;
 	}
 
+	private boolean isValidBracketsQuery(String data) {
+		//Query [Date, Time, Name, Name, (PreferenceType,PreferenceType,PreferenceType), Number]
+		data = data.replaceAll("\\s", ""); // get rid of spaces
+		int startCount = 0, commaCount = 0, endCount = 0;
+		int roundStart = 0, roundEnd = 0;
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) == '[') {
+				startCount++;
+			} else if (data.charAt(i) == '(') {
+				if (commaCount != 4 || startCount != roundStart-1) {
+					return false;
+				}
+				roundStart++;
+			} else if (data.charAt(i) == ')') {
+				if (roundStart -1 != roundEnd || commaCount != 6) {
+					return false;
+				}
+				roundEnd++;
+			} else if (data.charAt(i) == ',') {
+				commaCount++;
+			} else if (data.charAt(i) == ']') {
+				if (commaCount == 7 && startCount-1 == endCount && roundStart-1 == commaCount) {
+					endCount++;
+					commaCount = 0;
+				} else {
+					return false;
+				}
+			}
+		}
+		if (startCount != endCount || commaCount != 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	// e.g. Valid Query:
+	// [Date, Time, Name, Name, (PrefType1, PrefType2, PrefType3), numToDisplay]
+	/**
+	 * Parses the query given as an array of String tokens, adding to the queryList
+	 * iff the data is valid, otherwise prints the respective error message to screen and continues
+	 * adding as many valid queries as supplied
+	 * @precondition each set of tokens representing 1 query (8 tokens) are in a valid format.
+	 * @param lineTokens array of tokens from the input queries
+	 */
+	private void parseValidQuery(String[] lineTokens) {
+		for (int i = 0; i < lineTokens.length; i += 7) {
+			//get the date, splitting first token with delimiter '/'
+			String[] tmpTokens = lineTokens[i].split("/");
+			int day = Integer.parseInt(tmpTokens[0]);
+			int month = Integer.parseInt(tmpTokens[1])-1; //months from 0 to 11
+			int year = Integer.parseInt(tmpTokens[2]);
+			// then gets the hour and minutes
+			tmpTokens = lineTokens[i+1].split("[:]");
+			int hour = Integer.parseInt(tmpTokens[0]);
+			int min = Integer.parseInt(tmpTokens[1]);
+			
+			//check validity of date and time values
+			if (!validDateTime(day, month ,year, hour, min)){
+				System.out.print("Invalid date/time in entry: ");
+				// re-generate the input line
+				System.out.println("[" + day + "/"+ month + "/" + year + ", " + 
+								   min + ":" + hour + ", " + lineTokens[i+2] + 
+								   ", " + lineTokens[i+3] + ", (" + lineTokens[i+4] +
+								   ", " + lineTokens[i+5] + ", " + lineTokens[i+6] + 
+								   "), " + lineTokens[i+7] + "]");
+				continue;
+			} 
+			// set up Calendar object
+			Calendar dateTime = Calendar.getInstance();
+			dateTime.set(Calendar.DAY_OF_MONTH, day);
+			dateTime.set(Calendar.MONTH, month);
+			dateTime.set(Calendar.YEAR, year);
+			dateTime.set(Calendar.HOUR_OF_DAY, hour);
+			dateTime.set(Calendar.MINUTE, min);
+			
+			String[] prefOrder = new String[3];
+			prefOrder[0] = lineTokens[i+4];
+			prefOrder[1] = lineTokens[i+5];
+			prefOrder[2] = lineTokens[i+6];
+
+			int numToDisplay = Integer.parseInt(lineTokens[i+7]);
+			
+			Query newQuery = new Query(dateTime, lineTokens[i+2], lineTokens[i+3],
+									   prefOrder, numToDisplay);
+			queryList.add(newQuery);
+		}
+	}
+	
 	/**
 	 * Contains two checks. First uses the function verifyFlightFormat, this is to check formatting in
 	 * general. Fail the program and returns "incorrectly formatted flight data". However, in the case
@@ -240,7 +389,7 @@ public class TravelPlan {
 		return true;
 	}
 	
-	private boolean isValidBrackets(String data) {
+	private boolean isValidBracketsFlights(String data) {
 		//makes sure that the brackets are there and inbetween each open and
 		//close bracket, there will be 6 commas to divide it all
 		//Flight -> [ Date, Time, Name, Name, Duration, Name, Number ]
