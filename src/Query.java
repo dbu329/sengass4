24f3cc9b90f646b25a6ef98902aa561ddbce0818
Benjamin Mo <b.mo96@hotmail.com>
2015-05-19 10:29:31 +1000

    Set up initial fields/getters/setters

--- src/Query.java
+++ src/Query.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
 
 public class Query {
-
+	private Calendar departureTime; // date as well, might need to change
+	private String origin;
+	private String destination;
+	private String[] preferences;
+	private int numToDisplay;
+	private ArrayList<State> finalStates;
+	
+	public Query(Calendar time, String start, String end,
+				 String[] order, int amount) {
+		departureTime = time; // might have to change to clone
+		origin = start;
+		destination = end;
+		preferences = new String[3];
+		preferences = order; // again might have to clone
+		numToDisplay = amount;
+	}
+	
+	// should we split into get start year, month, date, time?
+	public Calendar getDepartureTime() {
+		return departureTime;
+	}
+	
+	public String getOrigin() {
+		return origin;
+	}
+	
+	public String getDestination() {
+		return destination;
+	}
+	
+	public String[] getPreferences() {
+		return preferences;
+	}
+	
+	public int getNumToDisplay() {
+		return numToDisplay;
+	}
+	
+	public ArrayList<State> getFinalStates() {
+		return finalStates;
+	}
+	
+	public void addFinalState(State state) {
+		finalStates.add(state);
+	}
 }
