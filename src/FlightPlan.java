1545838078a422684cb1d96a691ede26f142bc6a
snjay <sanjay.narayana@hotmail.com>
2015-05-30 16:56:15 +1000

    Added fields for FlightPlan, wrote toString() function

--- src/FlightPlan.java
+++ src/FlightPlan.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 
 public class FlightPlan {
 
+	private ArrayList<Flight> flightPlan;
+	
+	public FlightPlan(ArrayList<Flight> flights) {
+		this.flightPlan = flights;
+	}
+	
+	public String toString() {
+		return this.flightPlan.toString();
+	}
+	
 }
