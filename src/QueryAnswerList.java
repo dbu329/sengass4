90efd22956e2d774bd7b6a5bf4c51558b05442f9
snjay <sanjay.narayana@hotmail.com>
2015-05-30 16:48:32 +1000

    Added fields for QueryAnswerList

--- src/QueryAnswerList.java
+++ src/QueryAnswerList.java
@@ -1,3 +1,14 @@
 public class QueryAnswerList {
 	
+	Query query;
+	Answer answer;
+	
+	QueryAnswerList(Query query, Answer answer) {
+		this.query = query;
+		this.answer = answer;
+	}
+	
+	public String toString() {
+		return "(" + this.query + "," + "[" + this.answer + "]" + ")";
+	}
 }
