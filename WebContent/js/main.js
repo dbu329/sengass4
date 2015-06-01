7e9c3e627e4841b1b7921f4cae30813f3f93566c
Joshua Morton <joshanator1995@gmail.com>
2015-06-01 23:56:28 +1000

    added datepicker, order by and items per search to ui

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
 	var resultTmpl = _.template('\
-		<%= origin %> to <%= destination %> in <%= duration %> minutes\
+		<b><%= origin %></b> to <b><%= destination %></b> in <%= duration %> minutes\
 		<span class="arrow">&#9660;</span>\
-		<div class="details"></div>\
+		<div class="details"><%= airline %></div>\
 	');
-	
-	$('#test').prop('checked', true);
-	
-	
+	var cities = [
+	    'Sydney',
+	    'Canberra',
+	    'Adelaide',
+	    'Egypt'
+	];
+	$('#origin').autocomplete({ source: cities });
+	$('#destination').autocomplete({ source: cities });
+	$('#date').datepicker({ dateFormat: 'dd/mm/yy' });
 	$('#queryBtn').click(function (event) {
 		event.preventDefault();
 		$.post('query', {
@@ -17,13 +22,19 @@ $(document).ready(function () {
 			$('.results').empty();
 			results.forEach(function (i) {
 				var result = $('<div>');
-				result.addClass('result');
-				result.html(resultTmpl(i));
-				result.click(function () {
+				$(result).addClass('result');
+				$(result).html(resultTmpl(i));
+				$('.details', result).height(0);
+				$('.details', result).hide();
+				$(result).click(function () {
+					$('.details', result).show();
 					var height = $('.details', result).height();
 					$('.details', result).animate({
 						height: height == 100 ? 0 : 100
-					}, 200);
+					}, 200, function () {
+						if ($('.details', result).height() == 0)
+							$('.details', result).hide();
+					});
 					console.log($('span.arrow .details', result));
 					$('span.arrow', result).toggleClass('down');
 				});
